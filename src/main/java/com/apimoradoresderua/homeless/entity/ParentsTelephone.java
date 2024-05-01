cef0bc27d0cb5b7540c88079b7cf12ab1e7fdc10
lucassilvap <lucassilvap148@gmail.com>
2024-05-01 17:12:33 -0300

    fixed findTelephonesParetensByNameHomeless

--- src/main/java/com/apimoradoresderua/homeless/entity/ParentsTelephone.java
+++ src/main/java/com/apimoradoresderua/homeless/entity/ParentsTelephone.java
@@ -22,6 +22,14 @@ public class ParentsTelephone {
 	@ManyToOne
 	private HomelessPersonEntity homelessPersonEntity;
 	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getNumber() {
 		return number;
 	}
